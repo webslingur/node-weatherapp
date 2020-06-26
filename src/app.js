3616ecd993dca195e2b0e58ce3716e8baeda2a11
websleengur <mutsya@mutsyas-iMac.local>
2020-06-26 01:00:49 -0700

    port

--- src/app.js
+++ src/app.js
@@ -98,6 +98,6 @@ app.get("*", (req, res) => {
     });
 });
 
-app.listen(3001, () => {
-    console.log("Server is up on port 3000");
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
